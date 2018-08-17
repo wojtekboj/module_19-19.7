e82fa0adc96e6f5b23fd9b3b869da84055dd9879
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-08-17 20:25:54 +0200

    Modification Comment.js

--- redux-demo/src/Comment.js
+++ redux-demo/src/Comment.js
@@ -5,7 +5,6 @@ const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) => (
     {text} <span>votes: {votes}</span> <button onClick={() => thumbUpComment(id)}>Thumb up</button>
     {text} <span>votes: {votes}</span> <button onClick={() => thumbDownComment(id)}>Thumb Down</button>
 </li>
-
 );
 
 export default Comment;
