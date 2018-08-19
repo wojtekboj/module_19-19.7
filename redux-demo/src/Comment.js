383fa47fd77f14a44d6c176774ced0a48d13d782
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-08-19 10:00:54 +0200

    Modification Comment.js

--- redux-demo/src/Comment.js
+++ redux-demo/src/Comment.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) => (
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) =>
 <li>
-    {text} <span>votes: {votes}</span> <button onClick={() => thumbUpComment(id)}>Thumb up</button>
-    {text} <span>votes: {votes}</span> <button onClick={() => thumbDownComment(id)}>Thumb Down</button>
-</li>
-);
+    <span className="comment">{text}</span> <span>votes: {votes}</span>
+    <button onClick={() => thumbUpComment(id)}>Thumb up</button>
+    <button onClick={() => thumbDownComment(id)}>Thumb down</button>
+</li>;
 
 export default Comment;
